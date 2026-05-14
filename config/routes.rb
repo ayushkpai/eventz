85d47b9aeea44e5b0c617562d8f015261cb80176
Ayush Pai <ayushpai@ayushpai.com>
2026-05-14 11:42:21 +0530

    create records

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,5 @@ Rails.application.routes.draw do
   root "events#index"
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "edit_event"
-  patch "events/:id" => "events#update"
+  resources :events
 end
